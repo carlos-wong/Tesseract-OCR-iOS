299292367b8a3e4fc4e452f32258631b28e3017b
carlos <huaixian.huang@gmail.com>
2013-12-23 15:17:28 +0800

    add battery percent image and DC and usb capture the screen

--- Template Framework Project/Template Framework Project/G8ViewController.h
+++ Template Framework Project/Template Framework Project/G8ViewController.h
@@ -9,5 +9,14 @@
 #import <UIKit/UIKit.h>
 
 @interface G8ViewController : UIViewController
+{
+    IBOutlet UIImageView *batterPerecent;
+    IBOutlet UILabel *mylabel;
+
+}
+- (UIImage *)captureScreen;
+@property(nonatomic,retain)IBOutlet UIImageView *batterPerecent;
+@property(nonatomic,retain)IBOutlet UILabel *mylabel;
+
 
 @end
--- Template Framework Project/Template Framework Project/G8ViewController.m
+++ Template Framework Project/Template Framework Project/G8ViewController.m
@@ -8,12 +8,103 @@
 
 #import "G8ViewController.h"
 #import <TesseractOCR/TesseractOCR.h>
+#import <QuartzCore/QuartzCore.h>
+
 
 @interface G8ViewController ()
 
 @end
 
 @implementation G8ViewController
+
+@synthesize batterPerecent;
+
+
+- (void)image:(UIImage *)image didFinishSavingWithError:(NSError *)error contextInfo:(void *)contextInfo
+{
+    UIAlertView *alert;
+    
+    
+    if (error)
+        alert = [[UIAlertView alloc] initWithTitle:@"Error"
+                                           message:@"Unable to save image to Photo Album."
+                                          delegate:self cancelButtonTitle:@"Ok"
+                                 otherButtonTitles:nil];
+    else
+        alert = [[UIAlertView alloc] initWithTitle:@"Success"
+                                           message:@"Image saved to Photo Album."
+                                          delegate:self cancelButtonTitle:@"Ok"
+                                 otherButtonTitles:nil];
+    [alert show];
+}
+- (UIImage *)captureScreen
+{
+    // Create a graphics context with the target size
+    // On iOS 4 and later, use UIGraphicsBeginImageContextWithOptions to take the scale into consideration
+    // On iOS prior to 4, fall back to use UIGraphicsBeginImageContext
+    CGSize imageSize = [[UIScreen mainScreen] bounds].size;
+    if (NULL != UIGraphicsBeginImageContextWithOptions)
+        UIGraphicsBeginImageContextWithOptions(imageSize, NO, 0);
+    else
+        UIGraphicsBeginImageContext(imageSize);
+    
+    CGContextRef context = UIGraphicsGetCurrentContext();
+    
+    // Iterate over every window from back to front
+    for (UIWindow *window in [[UIApplication sharedApplication] windows])
+    {
+        if (![window respondsToSelector:@selector(screen)] || [window screen] == [UIScreen mainScreen])
+        {
+            // -renderInContext: renders in the coordinate space of the layer,
+            // so we must first apply the layer's geometry to the graphics context
+            CGContextSaveGState(context);
+            // Center the context around the window's anchor point
+            CGContextTranslateCTM(context, [window center].x, [window center].y);
+            // Apply the window's transform about the anchor point
+            CGContextConcatCTM(context, [window transform]);
+            // Offset by the portion of the bounds left of and above the anchor point
+            CGContextTranslateCTM(context,
+                                  -[window bounds].size.width * [[window layer] anchorPoint].x,
+                                  -[window bounds].size.height * [[window layer] anchorPoint].y);
+            
+            // Render the layer hierarchy to the current context
+            [[window layer] renderInContext:context];
+            
+            // Restore the context
+            CGContextRestoreGState(context);
+        }
+    }
+    
+    // Retrieve the screenshot image
+    UIImage *image = UIGraphicsGetImageFromCurrentImageContext();
+    
+    UIGraphicsEndImageContext();
+//    UIImageWriteToSavedPhotosAlbum(image, self, @selector(image:didFinishSavingWithError:contextInfo:), nil);
+    NSString  *pngPath = [NSHomeDirectory() stringByAppendingPathComponent:@"Documents/Test.png"];
+    NSString  *jpgPath = [NSHomeDirectory() stringByAppendingPathComponent:@"Documents/Test.jpg"];
+    
+    // Write a UIImage to JPEG with minimum compression (best quality)
+    // The value 'image' must be a UIImage object
+    // The value '1.0' represents image compression quality as value from 0.0 to 1.0
+    [UIImageJPEGRepresentation(image, 1.0) writeToFile:jpgPath atomically:YES];
+    
+    // Write image to PNG
+    [UIImagePNGRepresentation(image) writeToFile:pngPath atomically:YES];
+    
+    // Let's check to see if files were successfully written...
+    
+    // Create file manager
+    NSError *error;
+    NSFileManager *fileMgr = [NSFileManager defaultManager];
+    
+    // Point to Document directory
+    NSString *documentsDirectory = [NSHomeDirectory() stringByAppendingPathComponent:@"Documents"];
+    
+    // Write out the contents of home directory to console
+    NSLog(@"Documents directory: %@", [fileMgr contentsOfDirectoryAtPath:documentsDirectory error:&error]);
+
+    return image;
+}
 
 /****README****/
 /*
@@ -24,8 +115,14 @@
  
  Follow the readme at https://github.com/gali8/Tesseract-OCR-iOS for first step.
  */
-
- 
+-(UIImage *) captureImageOfView:(UIView *)srcView
+{
+    UIGraphicsBeginImageContext(srcView.bounds.size);
+    [srcView.layer renderInContext:UIGraphicsGetCurrentContext()];
+    UIImage *anImage = UIGraphicsGetImageFromCurrentImageContext();
+    UIGraphicsEndImageContext();
+    return anImage;
+}
 
 - (void)viewDidLoad
 {
@@ -40,9 +137,17 @@
 	[tesseract setImage:[UIImage imageNamed:@"image_sample.jpg"]]; //image to check
 	[tesseract recognize];
 	
-	NSLog(@"%@", [tesseract recognizedText]);
-	
+	NSLog(@"hello carlos:%@", [tesseract recognizedText]);
 	[tesseract clear];
+    UIImageWriteToSavedPhotosAlbum([self captureImageOfView:self.view],nil,nil,nil);
+//    [self captureScreen];
+    
+//    NSString  *pngPath = [NSHomeDirectory() stringByAppendingPathComponent:@"Documents/example.jpeg"];
+//    NSLog(@"png path is: %@",pngPath);
+//    UIImage* image = [UIImage imageNamed:pngPath];
+//    batterPerecent.image = image;
+
+
 }
 
 - (void)didReceiveMemoryWarning
