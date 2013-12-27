b71890efad2e5248f1de78856223ddcbe938d686
carlos <huaixian.huang@gmail.com>
2013-12-27 09:13:18 +0800

    clean the code

--- Template Framework Project/Template Framework Project/G8ViewController.m
+++ Template Framework Project/Template Framework Project/G8ViewController.m
@@ -24,7 +24,7 @@
 {
     NSLog(@"some on press the button");
     CGRect tempRect;
-    UIImage *tempImage = [self crop:tempRect];
+    UIImage *tempImage = [self captureScreenWithRect:tempRect];
 
 //    UIImageWriteToSavedPhotosAlbum(tempImage,nil,nil,nil);
 
@@ -38,58 +38,18 @@
 
     
 }
-
-
-
-
-- (void)image:(UIImage *)image didFinishSavingWithError:(NSError *)error contextInfo:(void *)contextInfo
-{
-    UIAlertView *alert;
-    
-    
-    if (error)
-        alert = [[UIAlertView alloc] initWithTitle:@"Error"
-                                           message:@"Unable to save image to Photo Album."
-                                          delegate:self cancelButtonTitle:@"Ok"
-                                 otherButtonTitles:nil];
-    else
-        alert = [[UIAlertView alloc] initWithTitle:@"Success"
-                                           message:@"Image saved to Photo Album."
-                                          delegate:self cancelButtonTitle:@"Ok"
-                                 otherButtonTitles:nil];
-    [alert show];
+- (UIImage *)captureScreenWithRect:(CGRect)rect {
+    rect.origin.x = (2*[[UIApplication sharedApplication] statusBarFrame].size.width)*3/4;
+    rect.origin.y = 0;
+    rect.size.width = (2*[[UIApplication sharedApplication] statusBarFrame].size.width)*1/4;
+    rect.size.height = 2* [[UIApplication sharedApplication] statusBarFrame].size.height;
+    CGImageRef image = UIGetScreenImage();
+    CGImageRef imageRef = CGImageCreateWithImageInRect(image, rect);
+    UIImage *result = [UIImage imageWithCGImage:imageRef];
+    CGImageRelease(imageRef);
+    return result;
 }
-- (UIImage *)captureScreen
-{
-    
-    // Retrieve the screenshot image
-    UIImage *image = [self captureImageOfView:self.view];
-    //    UIImageWriteToSavedPhotosAlbum(image, self, @selector(image:didFinishSavingWithError:contextInfo:), nil);
-    NSString  *pngPath = [NSHomeDirectory() stringByAppendingPathComponent:@"Documents/Test.png"];
-    NSString  *jpgPath = [NSHomeDirectory() stringByAppendingPathComponent:@"Documents/Test.jpg"];
-    
-    // Write a UIImage to JPEG with minimum compression (best quality)
-    // The value 'image' must be a UIImage object
-    // The value '1.0' represents image compression quality as value from 0.0 to 1.0
-    [UIImageJPEGRepresentation(image, 1.0) writeToFile:jpgPath atomically:YES];
-    
-    // Write image to PNG
-    [UIImagePNGRepresentation(image) writeToFile:pngPath atomically:YES];
-    
-    // Let's check to see if files were successfully written...
-    
-    // Create file manager
-    NSError *error;
-    NSFileManager *fileMgr = [NSFileManager defaultManager];
-    
-    // Point to Document directory
-    NSString *documentsDirectory = [NSHomeDirectory() stringByAppendingPathComponent:@"Documents"];
-    
-    // Write out the contents of home directory to console
-    NSLog(@"Documents directory: %@", [fileMgr contentsOfDirectoryAtPath:documentsDirectory error:&error]);
 
-    return image;
-}
 
 /****README****/
 /*
@@ -100,127 +60,6 @@
  
  Follow the readme at https://github.com/gali8/Tesseract-OCR-iOS for first step.
  */
--(UIImage *) captureImageOfView:(UIView *)srcView
-{
-    UIGraphicsBeginImageContext(srcView.bounds.size);
-    [srcView.layer renderInContext:UIGraphicsGetCurrentContext()];
-    UIImage *anImage = UIGraphicsGetImageFromCurrentImageContext();
-    UIGraphicsEndImageContext();
-    return anImage;
-}
-
--(UIImage *) captureRectOfScreen:(CGRect) rect
-{
-    UIImage *wholeScreen = [self screenshot];
-    
-    //Add status bar height
-    rect.origin.y += UIInterfaceOrientationIsLandscape([UIApplication sharedApplication].statusBarOrientation) ? [[UIApplication sharedApplication] statusBarFrame].size.width : [[UIApplication sharedApplication] statusBarFrame].size.height;
-    
-    //NSLog(@"%@",NSStringFromCGSize([wholeScreen size]));
-    
-    CGFloat scale = wholeScreen.scale;
-    
-    rect.origin.x *= scale;
-    rect.origin.y *= scale;
-    rect.size.width *= scale;
-    rect.size.height *= scale;
-    
-    UIImage *cropped = [UIImage imageWithCGImage:CGImageCreateWithImageInRect([wholeScreen CGImage], rect) scale:wholeScreen.scale orientation:wholeScreen.imageOrientation];
-    
-    //NSLog(@"Whole Screen Capt :%@ Scale: %f",NSStringFromCGSize([wholeScreen size]), wholeScreen.scale);
-    //NSLog(@"Rect to Crop :%@ Cropped :%@",NSStringFromCGRect(rect), NSStringFromCGSize([cropped size]));
-    
-    return cropped;
-}
--(UIImage *) screenshotold
-{
-    UIScreen *screen = [UIScreen mainScreen];
-    UIView *view = [screen snapshotViewAfterScreenUpdates:YES];
-    
-    UIGraphicsBeginImageContextWithOptions(screen.bounds.size, NO, 0);
-    [view drawViewHierarchyInRect:view.bounds afterScreenUpdates:YES];
-    UIImage *image = UIGraphicsGetImageFromCurrentImageContext();
-    UIGraphicsEndImageContext();
-    return image;
-}
-
-
-- (UIImage *) reSizeImage:(UIImage *)image toSize:(CGSize)reSize {
-    NSLog(@"srcimage is %@",image);
-    UIGraphicsBeginImageContext(CGSizeMake(reSize.width, reSize.height));
-    [image drawInRect:CGRectMake(0, 0, reSize.width, reSize.height)];
-    UIImage *reSizeImage = UIGraphicsGetImageFromCurrentImageContext();
-    UIGraphicsEndImageContext();
-    NSLog(@"reszieimage end");
-    return reSizeImage;
-}
-- (UIImage *)crop:(CGRect)rect {
-    rect.origin.x = (2*[[UIApplication sharedApplication] statusBarFrame].size.width)*3/4;
-    rect.origin.y = 0;
-    rect.size.width = (2*[[UIApplication sharedApplication] statusBarFrame].size.width)*1/4;
-    rect.size.height = 2* [[UIApplication sharedApplication] statusBarFrame].size.height;
-    CGImageRef imageRef = CGImageCreateWithImageInRect(UIGetScreenImage(), rect);
-    UIImage *result = [UIImage imageWithCGImage:imageRef];
-    CGImageRelease(imageRef);
-    return result;
-}
--(UIImage *) screenshot
-{
-    
-    CGImageRef screen = UIGetScreenImage();
-	UIImage* image = [UIImage imageWithCGImage:screen];
-    
-	CGImageRelease(screen);
-    return image;
-}
--(UIImage *) screenshotold2
-{
-    // Create a graphics context with the target size
-    // On iOS 4 and later, use UIGraphicsBeginImageContextWithOptions to take the scale into consideration
-    // On iOS prior to 4, fall back to use UIGraphicsBeginImageContext
-    CGSize imageSize = [[UIScreen mainScreen] bounds].size;
-    if (NULL != UIGraphicsBeginImageContextWithOptions)
-        UIGraphicsBeginImageContextWithOptions(imageSize, NO, 0);
-    else
-        UIGraphicsBeginImageContext(imageSize);
-    
-    CGContextRef context = UIGraphicsGetCurrentContext();
-    NSLog(@"run screenshot");
-    // Iterate over every window from back to front
-    for (UIWindow *window in [[UIApplication sharedApplication] windows])
-    {
-        NSLog(@"window is: %@",window);
-        if (![window respondsToSelector:@selector(screen)] || [window screen] == [UIScreen mainScreen])
-        {
-            NSLog(@"draw");
-            // -renderInContext: renders in the coordinate space of the layer,
-            // so we must first apply the layer's geometry to the graphics context
-            CGContextSaveGState(context);
-            // Center the context around the window's anchor point
-            CGContextTranslateCTM(context, [window center].x, [window center].y);
-            // Apply the window's transform about the anchor point
-            CGContextConcatCTM(context, [window transform]);
-            // Offset by the portion of the bounds left of and above the anchor point
-            CGContextTranslateCTM(context,
-                                  -[window bounds].size.width * [[window layer] anchorPoint].x,
-                                  -[window bounds].size.height * [[window layer] anchorPoint].y);
-            
-            // Render the layer hierarchy to the current context
-            [[window layer] renderInContext:context];
-            
-            // Restore the context
-            CGContextRestoreGState(context);
-        }
-    }
-    NSLog(@"run screenshot end");
-
-    // Retrieve the screenshot image
-    UIImage *image = UIGraphicsGetImageFromCurrentImageContext();
-    
-    UIGraphicsEndImageContext();
-    
-    return image;
-}
 
 - (void)viewDidLoad
 {
@@ -237,15 +76,6 @@
 	
 	NSLog(@"hello carlos:%@", [tesseract recognizedText]);
 	[tesseract clear];
-//    UIImageWriteToSavedPhotosAlbum([self captureImageOfView:self.view],nil,nil,nil);
-//    UIImageWriteToSavedPhotosAlbum([self screenshot],nil,nil,nil);
-
-//    [self captureScreen];
-    
-//    NSString  *pngPath = [NSHomeDirectory() stringByAppendingPathComponent:@"Documents/Test.png"];
-//    NSLog(@"png path is: %@",pngPath);
-//    UIImage* image = [UIImage imageNamed:pngPath];
-//    batterPerecent.image = image;
 
 }
 
